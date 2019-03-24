a2fcca00ef373a147a324bfd920e7db56dff4e3a
unknown <tarasikuss@gmail.com>
2019-03-24 16:27:25 +0300

    fix: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+solution(matrix);
+  return matrix;
 }
+function solution(matrix){
+  var row = 0;
+  var col = 0;
+  for (var i=0; i<matrix.length; i++){
+    for (var j=0; j<matrix[i].length; j++){
+      if (matrix[i][j]==0){
+        var mass = [];
+        for (var p=0;p<9;p++){
+          mass.push(matrix[p][j]);
+        }
+        row=i;
+        col=j;
+        for (var k=1; k<=9;k++){
+            if (matrix[i].indexOf(k)==-1&&mass.indexOf(k)==-1&&thirdAreaCheck(k,i,j,matrix)){
+              matrix[i][j]=k;
+            if (solution(matrix)) 
+              {
+                return true;
+              }
+            else
+              {
+                matrix[row][col] = 0;
+              }
+            }
+      }
+      return false;
+      }
+    }
+  }
+  return true;
+}
+function thirdAreaCheck(num, row, col, matrix)
+{
+  var thirdRowNum = Math.floor(row / 3) * 3;
+  var thirdColNum = Math.floor(col / 3) * 3;
+  for (var i = 0; i < 3; i++)
+  {
+    for (var j = 0; j < 3; j++)
+    {
+      if (matrix[thirdRowNum + i][thirdColNum + j] == num) return false;
+    };
+  };
+  return true;
+};
